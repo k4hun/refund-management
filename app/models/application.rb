81509f293a7a60e4ff2e1eb1bb6c0dfffb02b067
k4hun <thameth@gmail.com>
2015-12-11 09:46:34 +0000

    fix application model

--- app/models/application.rb
+++ app/models/application.rb
@@ -3,6 +3,5 @@ class Application < ActiveRecord::Base
     belongs_to :category
     
     validates :category, :user, presence: true
-    validates :refund_value, inclusion: { 1..100 }
     validates :description, length: { maximum: 500 }
 end
