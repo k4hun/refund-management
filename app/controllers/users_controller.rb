e4bf9c63bfb331f7af1a4e54d96e95c429a7ea71
k4hun <thameth@gmail.com>
2015-12-11 10:23:26 +0000

    add user controller with create action

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+    
+  end
+  
+  def create
+    @user = User.new(user_params)
+    @user.role = Role.find_by_name('user')
+    if @user.save
+      redirect_to users_path, notice: 'User created'
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
