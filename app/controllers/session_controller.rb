b7682bd6da1fa98caac8836c3c10f604dfd03084
k4hun <thameth@gmail.com>
2015-12-13 19:41:44 +0100

    change redirection path after login

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -7,7 +7,7 @@ class SessionController < ApplicationController
     if auth
       session[:user_id] = auth.id
       flash[:notice] = 'Logged in!'
-      redirect_to root_path
+      redirect_to session[:return_to] || root_path
     else
       redirect_to login_path, alert: 'Wrong user name or password!'
     end
