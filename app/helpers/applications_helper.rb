14dbe006579344a20c75debfcc131a52caeabcb9
k4hun <thameth@gmail.com>
2015-12-13 18:19:46 +0100

    display status of application

--- app/helpers/applications_helper.rb
+++ app/helpers/applications_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationsHelper
+	def check_status(app)
+		if app.status == 'rejected'
+			return 'Rejected'
+		elsif app.status == 'approved'
+			return "Approved with #{app.refund_value}% refund"
+		else 
+			return 'Pending'
+		end				
+	end
 end
