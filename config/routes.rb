5dc0cb788ca399074dbd922f3b9a637719a3de97
k4hun <thameth@gmail.com>
2015-12-13 19:40:00 +0100

    change root path

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   delete '/logout' => 'session#destroy', as: 'logout'
   resources :session, only: [:create]
   
-  root 'applications#new'
+  root 'applications#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
