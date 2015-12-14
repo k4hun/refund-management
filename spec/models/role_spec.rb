e4d09b98f117b0fd9049ae40d9048220a810ed3c
k4hun <thameth@gmail.com>
2015-12-14 10:06:47 +0000

    specs for role model

---
+++ spec/models/role_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Role, type: :model do
+    subject do
+		Role.new(
+			name: "Foo"
+		)
+	end
+	
+	it { should respond_to :name }
+	it { should respond_to :users }
+	
+	describe 'validations' do
+    it { is_expected.to validate_presence_of(:name).with_message('is not included in the list') }
+    it { is_expected.to validate_inclusion_of(:name).in_array(%w(admin user)) }
+  end
+
+  describe 'database columns' do
+    it { should have_db_column :name }
+  end
+
+  describe 'associations' do
+    it { is_expected.to have_many :users }
+  end
+end
