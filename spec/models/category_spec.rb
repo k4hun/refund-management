feae36a50732b6155ae7190c115050450f893052
k4hun <thameth@gmail.com>
2015-12-14 10:01:25 +0000

    category model specs

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+    subject do
+		Category.new(
+			name: "Foo"
+		)
+	end
+	
+	it { should respond_to :name }
+	it { should respond_to :applications }
+	
+	describe 'validations' do
+    it { is_expected.to validate_presence_of :name }
+  end
+
+  describe 'database columns' do
+    it { should have_db_column :name }
+  end
+
+  describe 'associations' do
+    it { is_expected.to have_many :applications }
+  end
+end
