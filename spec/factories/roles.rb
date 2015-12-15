694ffdd256422de7512a127d42b1dbf70a7fc1f1
k4hun <thameth@gmail.com>
2015-12-15 14:43:34 +0000

    add admin factory

--- spec/factories/roles.rb
+++ spec/factories/roles.rb
@@ -2,4 +2,8 @@ FactoryGirl.define do
   factory :role do
     name       'user'
   end
+  
+  factory :admin_role, class: Role do
+    name        'admin'
+  end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,4 +4,10 @@ FactoryGirl.define do
     password    '123456'
     role
   end
+  
+  factory :admin, class: User do
+    email       'admin@mail.com'
+    password    'adminadmin'
+    association :role, factory: :admin_role
+  end
 end
