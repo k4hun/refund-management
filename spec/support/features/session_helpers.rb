6a6125e01408a880486c35c24cd9077a6fdfd046
k4hun <thameth@gmail.com>
2015-12-15 14:44:15 +0000

    add admin login spec helper

--- spec/support/features/session_helpers.rb
+++ spec/support/features/session_helpers.rb
@@ -8,6 +8,14 @@ module Features
       fill_in 'Password', with: user.password
       click_button 'Log in'
     end
+    
+    def admin_login
+      admin = FactoryGirl.create(:admin)
+      visit login_path
+      fill_in 'Email', with: admin.email
+      fill_in 'Password', with: admin.password
+      click_button 'Log in'
+    end
 
     def logout
       click_on 'Logout'
